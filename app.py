4d8914511cee4566741e2ecf4351a73a4b9a3176
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-13 19:02:22 +0800

    added flask session to save responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from surveys import surveys
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -7,25 +7,31 @@ app.config['SECRET_KEY']='secret'
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 @app.route('/')
 def start_page():
     survey = surveys.get("satisfaction")
     return render_template('start.html', survey=survey)
 
+
 @app.route('/thanks')
 def thanks_page():
+    raise
     return render_template('thanks.html')
+
+
+@app.route('/reset-responses', methods=['POST'])
+def reset_responses():
+    session['responses'] = []
+    return redirect('/questions/0')
 
 
 @app.route('/questions/<int:question_index>', methods = ['GET', 'POST'])
 def question_page(question_index):
     survey = surveys.get("satisfaction")
     questions = survey.questions
-    if question_index != len(responses):
-        question_index = len(responses)
+    if question_index != len(session['responses']):
+        question_index = len(session['responses'])
         flash('trying to access an invalid question')
         return redirect('/questions/' + str(question_index))
     if question_index >= len(questions):
@@ -33,7 +39,9 @@ def question_page(question_index):
     question = questions[question_index]
     if request.method == 'POST':
         answer = request.form['answer']
+        responses = session['responses']
         responses.append(answer)
+        session['responses'] = responses
         return redirect('/questions/' + str(question_index + 1))
     return render_template('question.html', question=question, question_index=question_index)
 
